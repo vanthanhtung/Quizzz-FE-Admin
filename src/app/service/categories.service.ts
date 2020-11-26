4d536ae13e121a6485aa7f07ee8e15f5e51d955a
Nghia <vunghia2000@gmail.com>
2020-11-26 14:57:32 +0700

    CRD categories

---
+++ src/app/service/categories.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+const url = "http://localhost:5432/categories";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CategoriesService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.http.get(url);
+  }
+
+  get(id: any): Observable<any> {
+    return this.http.get(`${url}/${id}`);
+  }
+
+  create(data: { name: String }): Observable<any> {
+    return this.http.post(url, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${url}/${id}`);
+  }
+}
