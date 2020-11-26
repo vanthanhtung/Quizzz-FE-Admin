0aab858b214dd7f8de21b1be45bcd621f4109dfd
JavaHater <vanthanhtung1562013@gmail.com>
2020-11-26 15:36:51 +0700

    ok3h

--- src/app/pages/create-quiz/create-quiz.component.ts
+++ src/app/pages/create-quiz/create-quiz.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-create-quiz',
   templateUrl: './create-quiz.component.html',
-  styleUrls: ['./create-quiz.component.css']
 })
 export class CreateQuizComponent implements OnInit {
+
+  quizes: any=[];
+
+   quiz= {
+    content:"",
+    type: "",
+    level:"",
+    score: 0,
+    is_active: true
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createNew(){}
 }
