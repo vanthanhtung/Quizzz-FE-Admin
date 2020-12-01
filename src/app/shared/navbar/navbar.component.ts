52529e7f76c4eebe557d1ed30812d746e78abdfa
Nghia <vunghia2000@gmail.com>
2020-12-01 16:03:10 +0700

    log out

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/co
 import { ROUTES } from '../../sidebar/sidebar.component';
 import { Router } from '@angular/router';
 import { Location} from '@angular/common';
+import { TokenStorageService } from 'app/_services/token-storage.service';
 
 @Component({
     moduleId: module.id,
@@ -19,7 +20,7 @@ export class NavbarComponent implements OnInit{
     public isCollapsed = true;
     @ViewChild("navbar-cmp", {static: false}) button;
 
-    constructor(location:Location, private renderer : Renderer2, private element : ElementRef, private router: Router) {
+    constructor(location:Location, private renderer : Renderer2, private element : ElementRef, private router: Router, private tokenStorage: TokenStorageService) {
         this.location = location;
         this.nativeElement = element.nativeElement;
         this.sidebarVisible = false;
@@ -90,6 +91,10 @@ export class NavbarComponent implements OnInit{
           navbar.classList.remove('bg-white');
         }
 
+      }
+      signOut(): any{
+        this.tokenStorage.signOut();
+        this.router.navigateByUrl("")
       }
 
 }
